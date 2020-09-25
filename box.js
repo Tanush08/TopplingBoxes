bec08c8a982b09da72224fe40861c766869dc9b1
Tanush08 <71077849+Tanush08@users.noreply.github.com>
2020-09-25 18:47:54 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -18,8 +18,10 @@ class Box {
     translate(pos.x, pos.y);
     rotate(angle);
     rectMode(CENTER);
+    strokeWeight(4);
+    stroke("green")
     fill(255);
     rect(0, 0, this.width, this.height);
     pop();
   }
-};
+}
